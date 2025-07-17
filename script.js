250eceeccef507ed19f5f04ff74d11664ddd328a
chinky23-tech <chinky0330@gmail.com>
2025-07-17 23:25:17 +0530

    added rest button

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 let counter = 0;
 document.getElementById('increase').addEventListener('click',function(){
-    counter += 1;
+    counter += 2;
 document.getElementById('count').textContent = counter;
+});
+
+document.getElementById('reset').addEventListener('click',function(){
+    counter = 0;
+    document.getElementById('count').textContent = counter;
 });
